326ee95afc8e72edd2ab8df20f3e8eec799d84ad
Takuya Yamauchi <tyama@preferred.jp>
2021-08-25 09:44:53 +0900

    Updated.

--- cem/p01.py
+++ cem/p01.py
@@ -2,7 +2,18 @@ import numpy as np
 
 def make_mass(p):
   q = p[:, 1:] - p[:, 0][..., np.newaxis]
-  return q
+
+  ax = np.array( [ [ 1, 2 ]
+                 , [ 2, 0 ]
+                 , [ 0, 1 ] ] )
+  cb = np.array( [ [ 1, 2, 0 ]
+                 , [ 2, 0, 1 ] ] )
+  n = np.zeros((3, 4), dtype=np.float64)
+  n[:,1:] = q[ax[:,0:1],cb[0]]*q[ax[:,1:2],cb[1]] \
+          - q[ax[:,1:2],cb[0]]*q[ax[:,0:1],cb[1]]
+  n[:,0] = -n[:,1:].sum(axis=-1)
+
+  return n
 
 if __name__ == '__main__':
   p = np.array \
@@ -12,8 +23,8 @@ if __name__ == '__main__':
   , dtype=np.float64 )
   print(make_mass(p))
   p = np.array \
-  ( [ [ 1, 2, 1, 1 ]
-    , [ 1, 1, 3, 1 ]
-    , [ 2, 1, 1, 4 ] ]
+  ( [ [ 0, 2, 1, 1 ]
+    , [ 1, 1, 2, 1 ]
+    , [ 1, 1, 1, 2 ] ]
   , dtype=np.float64 )
   print(make_mass(p))
