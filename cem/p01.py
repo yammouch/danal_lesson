e256ccf440f69184a29754cd364efe3b4c586026
Takuya Yamauchi <tyama@preferred.jp>
2021-08-26 08:29:34 +0900

    ntet returns the vol.

--- cem/p01.py
+++ cem/p01.py
@@ -11,7 +11,9 @@ def ntet(p): # p.shape: (3, 4)
   n[:,1:] = q[ax[0][:,None],cb[0]]*q[ax[1][:,None],cb[1]] \
           - q[ax[1][:,None],cb[0]]*q[ax[0][:,None],cb[1]]
   n[:,0] = -n[:,1:].sum(axis=-1) #; print(n)
-  return n
+  vol = (q[:,0]*n[:,1]).sum(axis=-1)
+  n /= vol
+  return n, vol
 
 def make_mass(n): # n.shape == (3, 4)
   cc = (  vp[[1,1,0]][...,None  ]
@@ -29,10 +31,14 @@ if __name__ == '__main__':
     , [ 0, 0, 1, 0 ]
     , [ 0, 0, 0, 1 ] ]
   , dtype=np.float64 )
-  print(make_mass(ntet(p)))
+  n, vol = ntet(p)
+  print(make_mass(n))
+
   p = np.array \
   ( [ [ 0, 2, 1, 1 ]
     , [ 1, 1, 2, 1 ]
     , [ 1, 1, 1, 2 ] ]
   , dtype=np.float64 )
-  print(make_mass(ntet(p)))
+  n, vol = ntet(p)
+  print(make_mass(n))
+  print(make_mass(n)*vol*vol)
