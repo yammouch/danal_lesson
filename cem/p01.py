2f3a145998a3ce8e5c13b31ac7e008831944a28a
Takuya Yamauchi <tyama@preferred.jp>
2021-08-25 10:29:02 +0900

    cc has been added.

--- cem/p01.py
+++ cem/p01.py
@@ -1,7 +1,10 @@
 import numpy as np
 
+vp = np.array( [ [ 0, 0, 0, 1, 1, 2 ]
+               , [ 1, 2, 3, 2, 3, 3 ] ], dtype=np.int64 )
+
 def make_mass(p):
-  q = p[:, 1:] - p[:, 0][..., np.newaxis] ; print(q)
+  q = p[:, 1:] - p[:, 0][..., np.newaxis] #; print(q)
 
   ax = np.array( [ [ 1, 2, 0 ]
                  , [ 2, 0, 1 ] ], dtype=np.int64 )
@@ -10,7 +13,9 @@ def make_mass(p):
   n = np.zeros((3, 4), dtype=np.float64)
   n[:,1:] = q[ax[0][:,np.newaxis],cb[0]]*q[ax[1][:,np.newaxis],cb[1]] \
           - q[ax[1][:,np.newaxis],cb[0]]*q[ax[0][:,np.newaxis],cb[1]]
-  n[:,0] = -n[:,1:].sum(axis=-1)
+  n[:,0] = -n[:,1:].sum(axis=-1) #; print(n)
+  cc = (  vp[[1,1,0]][...,np.newaxis]
+       == vp[[1,0,0]][...,np.newaxis,:] ) + 1 #; print(cc)
 
   return n
 
