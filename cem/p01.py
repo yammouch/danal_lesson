2ad2eb35c223d2364dbec8c8ddf622035207bec2
Takuya Yamauchi <tyama@preferred.jp>
2021-08-26 08:36:45 +0900

    Ready for batch.

--- cem/p01.py
+++ cem/p01.py
@@ -3,26 +3,24 @@ import numpy as np
 vp = np.array( [ [0,0,0,1,1,2]
                , [1,2,3,2,3,3] ], dtype=np.int64 )
 
-def ntet(p): # p.shape: (3, 4)
-  ax = np.array( [ [1,2,0] , [2,0,1] ], dtype=np.int64 )
-  cb = np.array( [ [1,2,0] , [2,0,1] ], dtype=np.int64 )
-  q = p[:,1:] - p[:,0][...,None] #; print(q)
+def ntet(p): # p.shape: (..., 3, 4)
+  q = p[...,1:] - p[...,0][...,None] #; print(q)
   n = np.zeros((3,4), dtype=np.float64)
-  n[:,1:] = q[ax[0][:,None],cb[0]]*q[ax[1][:,None],cb[1]] \
-          - q[ax[1][:,None],cb[0]]*q[ax[0][:,None],cb[1]]
-  n[:,0] = -n[:,1:].sum(axis=-1) #; print(n)
-  vol = (q[:,0]*n[:,1]).sum(axis=-1)
+  n[...,1:] = q[..., [[1],[2],[0]], [1,2,0]] * q[..., [[2],[0],[1]], [2,0,1]] \
+            - q[..., [[2],[0],[1]], [1,2,0]] * q[..., [[1],[2],[0]], [2,0,1]]
+  n[...,0] = -n[:,1:].sum(axis=-1) #; print(n)
+  vol = (q[...,0]*n[...,1]).sum(axis=-1)
   n /= vol
   return n, vol
 
-def make_mass(n): # n.shape == (3, 4)
+def make_mass(n): # n.shape: (..., 3, 4)
   cc = (  vp[[1,1,0]][...,None  ]
        == vp[[1,0,0]][...,None,:] ) + 1 #; print(cc)
-  ma  = (n[:,vp[0]][...,None]*n[:,vp[0]][...,None,:]).sum(axis=-3)*cc[0]
-  m1  = (n[:,vp[0]][...,None]*n[:,vp[1]][...,None,:]).sum(axis=-3)*cc[1]
+  ma  = (n[...,vp[0]][...,None]*n[...,vp[0]][...,None,:]).sum(axis=-3)*cc[0]
+  m1  = (n[...,vp[0]][...,None]*n[...,vp[1]][...,None,:]).sum(axis=-3)*cc[1]
   ma += m1
   ma += np.moveaxis(m1,-2,-1)
-  ma += (n[:,vp[1]][...,None]*n[:,vp[1]][...,None,:]).sum(axis=-3)*cc[2]
+  ma += (n[...,vp[1]][...,None]*n[...,vp[1]][...,None,:]).sum(axis=-3)*cc[2]
   return ma
 
 if __name__ == '__main__':
