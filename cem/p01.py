172d11b4f61afbbbf749824c789d1ace7938b15d
Takuya Yamauchi <tyama@preferred.jp>
2021-08-26 08:54:57 +0900

    Now make_mass multiplies vol/120.

--- cem/p01.py
+++ cem/p01.py
@@ -14,14 +14,18 @@ def ntet(p): # p.shape: (..., 3, 4)
   n /= vol[..., None, None]
   return n, vol
 
-def make_mass(n): # n.shape: (..., 3, 4)
-  cc = (  vp[[1,1,0]][...,None  ]
-       == vp[[1,0,0]][...,None,:] ) + 1 #; print(cc)
-  ma  = (n[...,vp[0]][...,None]*n[...,vp[0]][...,None,:]).sum(axis=-3)*cc[0]
-  m1  = (n[...,vp[0]][...,None]*n[...,vp[1]][...,None,:]).sum(axis=-3)*cc[1]
+def make_mass(n, vol): # n.shape: (..., 3, 4)
+  cc = (  vp[[1,1,0]][..., None   ]
+       == vp[[1,0,0]][..., None, :] ) + 1 #; print(cc)
+  ma  = ( n[..., vp[0]][..., None   ]
+        * n[..., vp[0]][..., None, :] ).sum(axis=-3) * cc[0]
+  m1  = ( n[..., vp[0]][..., None   ]
+        * n[..., vp[1]][..., None, :] ).sum(axis=-3) * cc[1]
   ma += m1
   ma += np.moveaxis(m1,-2,-1)
-  ma += (n[...,vp[1]][...,None]*n[...,vp[1]][...,None,:]).sum(axis=-3)*cc[2]
+  ma += ( n[..., vp[1]][..., None   ]
+        * n[..., vp[1]][..., None, :] ).sum(axis=-3) * cc[2]
+  ma *= vol[..., None, None]/120
   return ma
 
 if __name__ == '__main__':
@@ -34,8 +38,9 @@ if __name__ == '__main__':
       , [ 1, 1, 1, 2 ] ] ]
   , dtype=np.float64 )
   n, vol = ntet(p)
-  print(make_mass(n))
+  m = make_mass(n, vol)
   print(vol)
+  print(m / vol[..., None, None] * 120)
 
  #p = np.array \
  #( [ [ 0, 2, 1, 1 ]
