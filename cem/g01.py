e2a1ac9066e9f33d96527cac940bfa3924460e26
Takuya Yamauchi <tyama@preferred.jp>
2021-09-03 11:45:09 +0900

    First checkin.

---
+++ cem/g01.py
@@ -0,0 +1,16 @@
+import gmsh
+
+gmsh.initialize()
+
+p = [gmsh.model.occ.addPoint(*a)
+     for a in [(0,0,-1), (0,0,1), (2**0.5,-1,0), (2**0.5,1,0)] ]
+l = [gmsh.model.occ.addLine(*[p[i] for i in a])
+     for a in [(0,1), (0,2), (0,3), (1,2), (1,3), (2,3)] ]
+c = [gmsh.model.occ.addCurveLoop([l[i] for i in a])
+     for a in [(0,1,3), (0,2,4), (1,2,5), (3,4,5)] ]
+s = [gmsh.model.occ.addPlaneSurface([i]) for i in c]
+sl = gmsh.model.occ.addSurfaceLoop(s)
+v = gmsh.model.occ.addVolume([sl])
+
+gmsh.model.occ.synchronize()
+gmsh.fltk.run()
