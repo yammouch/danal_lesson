463a7a6bd235f3114cd0dabc461da98c75d57fc3
Takuya Yamauchi <tyama@preferred.jp>
2021-09-03 15:45:35 +0900

    A 1D physical group has been added.

--- cem/g01.py
+++ cem/g01.py
@@ -3,7 +3,7 @@ import gmsh
 gmsh.initialize()
 
 p = [gmsh.model.occ.addPoint(*a)
-     for a in [(0,0,-1), (0,0,1), (2**0.5,-1,0), (2**0.5,1,0)] ]
+     for a in [(0,0,-1), (2**0.5,-1,0), (2**0.5,1,0), (0,0,1)] ]
 l = [gmsh.model.occ.addLine(*[p[i] for i in a])
      for a in [(0,1), (0,2), (0,3), (1,2), (1,3), (2,3)] ]
 c = [gmsh.model.occ.addCurveLoop([l[i] for i in a])
@@ -13,10 +13,13 @@ sl = gmsh.model.occ.addSurfaceLoop(s)
 v = gmsh.model.occ.addVolume([sl])
 
 gmsh.model.occ.synchronize()
+isrc = gmsh.model.addPhysicalGroup(1, l[2:3])
 gmsh.model.mesh.setSize(gmsh.model.getEntities(0), 3)
 #gmsh.model.mesh.setSize(gmsh.model.getEntities(0), 1.5)
 gmsh.model.mesh.generate(3)
 print(gmsh.model.mesh.getNodes())
 print(gmsh.model.mesh.getElements())
+print(gmsh.model.getPhysicalGroups())
+print(gmsh.model.mesh.getNodesForPhysicalGroup(1, 1))
 #gmsh.write("g01.msh2")
 gmsh.fltk.run()
