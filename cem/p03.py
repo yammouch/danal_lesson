1aa585d8225d741d2d780f217601d7f28f3d1404
Takuya Yamauchi <tyama@preferred.jp>
2021-09-10 18:09:52 +0900

    Global matrix is constructed.

--- cem/p03.py
+++ cem/p03.py
@@ -2,9 +2,11 @@ import numpy as np
 import scipy.sparse
 import p01
 
+c  = 299792458.0
+u0 = 4e-7*np.pi
+e0 = 1/(u0*c**2)
+
 def solve(vrt, spt, edg, freq):
-  u0 = 4e-7*np.pi
-  e0 = 8.854e-12
   b = np.zeros((6, 6), dtype=np.complex128)
   for p, e in zip(spt, edg):
     n, area = p01.ntri(vrt[:, p])
@@ -30,6 +32,7 @@ def solve(vrt, spt, edg, freq):
 vp = np.array([[0,0,0,1,1,2],[1,2,3,2,3,3]])
 
 if __name__ == '__main__':
+  np.set_printoptions(precision=3)
   vrt = np.array \
   ( [ [ 0, 1, 0, 0, 1, 0 ]
     , [ 0, 0, 1, 0, 0, 1 ]
@@ -38,15 +41,23 @@ if __name__ == '__main__':
   ( [ [ 0, 1, 2, 3 ]
     , [ 1, 2, 3, 4 ]
     , [ 2, 3, 4, 5 ] ] )
-  v2e = np.unique(tet[:, np.moveaxis(vp,0,1)].reshape(-1,2), axis=0)
-  e2v = scipy.sparse.csr_matrix((np.arange(v2e.shape[0]), (v2e[:,0], v2e[:,1])))
-  print(v2e)
-  print(e2v)
- #spt = np.array([ [0,1,2], [0,1,3], [0,2,3], [1,2,3] ])
- #edg = np.array([ [0,1,3], [0,2,4], [1,2,5], [3,4,5] ])
- #freq = 1e3
- #np.set_printoptions(3)
- #for freq in [1e3, 10e3, 100e3, 1e6, 10e6, 100e6, 1e9, 10e9, 100e9]:
- #  sol = solve(vrt, spt, edg, freq)
- #  print(sol)
- #  print(sol[2]/(2*np.pi*freq))
+  e2v = np.unique(tet[:, np.moveaxis(vp,0,1)].reshape(-1,2), axis=0)
+  v2e = scipy.sparse.csr_matrix((np.arange(e2v.shape[0]), (e2v[:,0], e2v[:,1])))
+  coords = np.moveaxis(vrt[:,tet], 0, 1)
+  n, vol = p01.ntet(coords)
+  stiff = p01.make_stiff(n, vol)
+  mass = p01.make_mass(n, vol)
+  lhs = np.zeros((e2v.shape[0], e2v.shape[0]), dtype=np.complex128)
+  rhs = np.zeros((e2v.shape[0],), dtype=np.complex128)
+  freq = 1e6
+  for i, s, m in zip(tet, stiff, mass):
+    dst = v2e[tuple(i[vp])]
+    lhs[dst, dst.T] += s/u0
+    lhs[dst, dst.T] -= (2*np.pi*freq)**2*e0*m
+  print(lhs)
+  lhs[v2e[0,1]          ] = 0
+  lhs[v2e[0,1], v2e[0,1]] = 1
+  rhs[v2e[0,3]] = -2j*np.pi*freq
+  print(rhs)
+  sol = np.linalg.solve(lhs, rhs)
+  print(sol)
