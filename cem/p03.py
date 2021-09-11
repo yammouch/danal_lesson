44840565379db1a3a9eaef4b4f99eae886ccfcc3
Takuya Yamauchi <tyama@preferred.jp>
2021-09-11 11:30:29 +0900

    The capacitance is very precise!

--- cem/p03.py
+++ cem/p03.py
@@ -28,7 +28,6 @@ def solve(vrt, spt, edg, freq):
   sol = np.linalg.solve(lhs, rhs)
   return sol
 
-
 vp = np.array([[0,0,0,1,1,2],[1,2,3,2,3,3]])
 
 if __name__ == '__main__':
@@ -43,21 +42,27 @@ if __name__ == '__main__':
     , [ 2, 3, 4, 5 ] ] )
   e2v = np.unique(tet[:, np.moveaxis(vp,0,1)].reshape(-1,2), axis=0)
   v2e = scipy.sparse.csr_matrix((np.arange(e2v.shape[0]), (e2v[:,0], e2v[:,1])))
+ #print(v2e)
   coords = np.moveaxis(vrt[:,tet], 0, 1)
   n, vol = p01.ntet(coords)
   stiff = p01.make_stiff(n, vol)
   mass = p01.make_mass(n, vol)
   lhs = np.zeros((e2v.shape[0], e2v.shape[0]), dtype=np.complex128)
   rhs = np.zeros((e2v.shape[0],), dtype=np.complex128)
-  freq = 1e6
-  for i, s, m in zip(tet, stiff, mass):
-    dst = v2e[tuple(i[vp])]
-    lhs[dst, dst.T] += s/u0
-    lhs[dst, dst.T] -= (2*np.pi*freq)**2*e0*m
-  print(lhs)
-  lhs[v2e[0,1]          ] = 0
-  lhs[v2e[0,1], v2e[0,1]] = 1
-  rhs[v2e[0,3]] = -2j*np.pi*freq
-  print(rhs)
-  sol = np.linalg.solve(lhs, rhs)
-  print(sol)
+  for freq in [10e3, 100e3, 1e6]:
+    for i, s, m in zip(tet, stiff, mass):
+      dst = v2e[tuple(i[vp])]
+      lhs[dst, dst.T] += s/u0
+      lhs[dst, dst.T] -= (2*np.pi*freq)**2*e0*m
+    edge0 = np.array(v2e[ [0,0,1,    3,3,4]
+                        , [1,2,2,    4,5,5] ])[0]
+   #edge0 = np.array(v2e[ [0,0,1,1,2,3,3,4]
+   #                    , [1,2,2,4,5,4,5,5] ])[0]
+    lhs[edge0       ] = 0
+    lhs[edge0, edge0] = 1
+   #print(lhs)
+    rhs[v2e[0,3]] = -2j*np.pi*freq
+   #print(rhs)
+    sol = np.linalg.solve(lhs, rhs)
+    print(sol)
+    print(1/(e0*0.5*2*np.pi*freq))
