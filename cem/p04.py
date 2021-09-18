9c7f53c70b2eb0c74e24febef1b0d178bd7af71b
Takuya Yamauchi <tyama@preferred.jp>
2021-09-18 12:47:39 +0900

    local2global has been added.

--- cem/p04.py
+++ cem/p04.py
@@ -6,13 +6,18 @@ c  = 299792458.0
 u0 = 4e-7*np.pi
 e0 = 1/(u0*c**2)
 
+vp = np.array([[0,0,0,1,1,2],[1,2,3,2,3,3]])
+
+def local2global(glo, tet, v2e, loc): # inplace
+    for i, l in zip(tet, loc):
+        dst = v2e[tuple(i[vp])]
+        glo[dst, dst.T] += l
+
 def freq1(freq, tet, stiff, mass, e2v, v2e):
     lhs = np.zeros((e2v.shape[0], e2v.shape[0]), dtype=np.complex128)
     rhs = np.zeros((e2v.shape[0],), dtype=np.complex128)
-    for i, s, m in zip(tet, stiff, mass):
-        dst = v2e[tuple(i[vp])]
-        lhs[dst, dst.T] += s/u0
-        lhs[dst, dst.T] -= (2*np.pi*freq)**2*e0*m
+    local2global(lhs, tet, v2e, stiff/u0)
+    local2global(lhs, tet, v2e, -(2*np.pi*freq)**2*e0*mass)
     edge0 = np.array(v2e[ [0,0,1,    3,3,4]
                         , [1,2,2,    4,5,5] ])[0]
    #edge0 = np.array(v2e[ [0,0,1,1,2,3,3,4]
@@ -24,8 +29,6 @@ def freq1(freq, tet, stiff, mass, e2v, v2e):
    #print(rhs)
     sol = np.linalg.solve(lhs, rhs)
     return sol
-
-vp = np.array([[0,0,0,1,1,2],[1,2,3,2,3,3]])
 
 def main():
     np.set_printoptions(precision=3)
