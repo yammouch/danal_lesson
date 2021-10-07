c0c9124ef97401ccee07de3663d77cd0b65ca2a5
Takuya Yamauchi <tyama@preferred.jp>
2021-10-07 09:23:46 +0900

    Inductance is calculated.

--- cem/p06.py
+++ cem/p06.py
@@ -3,18 +3,23 @@ import p04
 
 def main():
     np.set_printoptions(precision=3)
+    w = 1
+    l = 3
+    h = 1
     vrt = np.array \
-    ( [ [ 0, 1, 0, 0, 1, 0 ]
-      , [ 0, 0, 1, 0, 0, 1 ]
-      , [ 0, 0, 0, 1, 1, 1 ] ] )
+    ( [ [ -w/2, -w/2, w/2,  w/2, -w/2, w/2 ]
+      , [    0,    0,   0,    0,    l,   l ]
+      , [ -h/2,  h/2, h/2, -h/2,    0,   0 ] ] )
     tet = np.array \
-    ( [ [ 0, 1, 2, 3 ]
-      , [ 1, 2, 3, 4 ]
-      , [ 2, 3, 4, 5 ] ] )
+    ( [ [ 0, 1, 3, 5 ]
+      , [ 1, 2, 3, 5 ]
+      , [ 0, 1, 4, 5 ] ] )
     tri = np.array \
-    ( [ [0, 1, 2]
-      , [3, 4, 5] ] )
-    lin = np.array( [ [0, 3] ] )
+    ( [ [0, 3, 5]
+      , [0, 4, 5]
+      , [1, 2, 5]
+      , [1, 4, 5] ] )
+    lin = np.array( [ [0, 1] ] )
     v2e, bwh = p04.edge_num_banded(tet)
     pgroups = [('e', ([0,0,1],), lin), ('b', (), tri), ('v', (), tet)]
     for freq in [10e3, 100e3, 1e6]:
@@ -23,7 +28,7 @@ def main():
         for ptype, attr, nodes in pgroups:
             if ptype == 'e':
                 print(p04.isrc_v(sol, vrt, nodes, v2e, attr[0]))
-        print(1/(p04.e0*0.5*2*np.pi*freq))
+        print(p04.u0*0.5*w*l/h*2*np.pi*freq)
 
 if __name__ == '__main__':
     main()
