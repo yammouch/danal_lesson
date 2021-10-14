81bda33077c6d645be5e115709c235261385a1ad
Takuya Yamauchi <tyama@preferred.jp>
2021-10-14 17:08:32 +0900

    First checkin.

--- cem/m1_l_e1.py
+++ cem/m1_l_e1.py
@@ -12,14 +12,17 @@ def main():
       , [ -h/2,  h/2,    0,   0 ] ] )
     tet = np.array \
     ( [ [ 0, 1, 2, 3 ] ] )
+    tri = np.array \
+    ( [ [ 0, 2, 3 ]
+      , [ 1, 2, 3 ] ] )
     lin = np.array( [ [0, 1] ] )
     v2e, bwh = p04.edge_num_banded(tet)
-    pgroups = [('e', ([0,0,1],), tet), ('v', (), tet)]
+    pgroups = [('e', ([0,0,1],), tet), ('v', (), tet), ('b', (), tri)]
     freq = 50
     sol = p04.solve_geom(freq, vrt, pgroups, v2e.nnz, v2e, bwh)
     print(sol)
     print(p04.isrc_v(sol, vrt, lin, v2e, [0,0,1]))
-    print(2*(1/freq)/4*(2/np.pi)/(4*np.pi*p04.e0*0.5*h))
+    print(p04.u0*0.5*w*l/h*2*np.pi*freq)
 
 if __name__ == '__main__':
     main()
