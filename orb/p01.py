71fbb99d114bafe17a546feac24e4b469030c769
Takuya Yamauchi <tyama@preferred.jp>
2022-07-22 08:46:05 +0900

    symmetrize() has been added.

--- orb/p01.py
+++ orb/p01.py
@@ -7,3 +7,10 @@ def kin(k_basis, lc):
     x = x.sum(axis=-1)
     x = -0.5*x
     return x
+
+def symmetrize(a):
+    for i in range(a.ndim):
+        l = a.shape[i]
+        if l % 2 == 0:
+            a[(slice(None),)*i + (l//2,)] *= 0.5
+    return a
--- orb/p01_test.py
+++ orb/p01_test.py
@@ -24,5 +24,25 @@ def test_kin():
         print('[ER] test_kin')
         print(result)
 
+@add_test
+def test_symmetrize():
+    x = np.array \
+    ( [ [  0,  2,  4,  6 ]
+      , [  8, 10, 12, 14 ]
+      , [ 16, 18, 20, 22 ]
+      , [ 24, 26, 28, 30 ] ]
+    , dtype=np.float64 )
+    expc = np.array \
+    ( [ [  0,  2,  2,  6 ]
+      , [  8, 10,  6, 14 ]
+      , [  8,  9,  5, 11 ]
+      , [ 24, 26, 14, 30 ] ] )
+    dut.symmetrize(x)
+    if (np.abs(x - expc) < 1e-3).all():
+        print("[OK] test_symmetrize")
+    else:
+        print("[ER] test_symmetrize")
+        print(x)
+
 for _, f in tests:
     f()
