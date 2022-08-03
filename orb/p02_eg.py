49f94f49598e1e5dee53e27275b8ad978d30da47
Takuya Yamauchi <tyama@preferred.jp>
2022-08-03 09:53:59 +0900

    hydrogen() has been added.

--- orb/p02_eg.py
+++ orb/p02_eg.py
@@ -25,10 +25,30 @@ def orb_3d():
     e, v = p02.solve_1elec(basis_nwn, box_size, vext)
     print(e[:20])
 
+def hydrogen_vext(npoint_half, size):
+    side = 0.5*size*(1-1/(2*npoint_half))
+    dist = np.linspace(-side, side, 2*npoint_half)
+    d2 = dist**2
+    d2_3d = d2[:, None, None] + d2[None, :, None] + d2[None, None, :]
+    vext = d2_3d**0.5
+    vext = -1.0/vext
+    return vext
+
+def hydrogen():
+    npoint_half = 12
+    size = 8
+    sl = slice(-npoint_half//2, npoint_half//2 + 1)
+    basis_nwn = np.mgrid[sl, sl, sl].reshape(3, -1).T
+    box_size = np.full(3, size, dtype=float)
+    vext = hydrogen_vext(npoint_half, size)
+    e, v = p02.solve_1elec(basis_nwn, box_size, vext)
+    print(e[:20])
+
 def main():
     orb_1d()
     orb_2d()
     orb_3d()
+    hydrogen()
 
 if __name__ == '__main__':
     main()
