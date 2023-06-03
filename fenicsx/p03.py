0afd499ac8a989f1274802f88adfd3ef2ebb34f5
Takuya Yamauchi <tyama@preferred.jp>
2023-06-03 11:55:25 +0900

    First checkin.

---
+++ fenicsx/p03.py
@@ -0,0 +1,31 @@
+import dolfinx
+import ufl
+import mpi4py
+import numpy as np
+import itertools
+
+msh = dolfinx.mesh.create_mesh \
+( mpi4py.MPI.COMM_SELF
+, [[0,1,2],[0,3,2]]
+, [[0,0],[1,0],[0,2],[1,2]]
+, ufl.Mesh(ufl.Cell("triangle", 2) ) )
+
+print(msh.geometry.x)
+
+for i, j in itertools.product(range(3), range(3)):
+    msh.topology.create_connectivity(i, j)
+
+for i in [1, 2]:
+    print(msh.topology.connectivity(i, 0))
+
+h1 = dolfinx.fem.FunctionSpace(msh, ('CG', 1))
+print(h1.dofmap.dof_layout.entity_dofs(0, 2))
+print(h1.dofmap.dof_layout.entity_dofs(2, 0))
+print(h1.dofmap.dof_layout.entity_closure_dofs(0, 2))
+print(h1.dofmap.dof_layout.entity_closure_dofs(2, 0))
+
+h1_f = dolfinx.fem.Function(h1)
+print(h1_f.x.array)
+
+h1_f.x.array[h1.dofmap.dof_layout.entity_dofs(0, 2)] = [1]
+print(h1_f.x.array)
