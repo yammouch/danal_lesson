435d0b87dc6ddf42f3a422e6eb11700c7a940dc2
Takuya Yamauchi <tyama@preferred.jp>
2023-08-11 16:40:42 +0900

    Being debugged.

---
+++ fenicsx/p06.py
@@ -0,0 +1,39 @@
+import itertools
+import numpy as np
+import basix
+
+pts, wts = basix.make_quadrature(basix.CellType.triangle, 4)
+poly = basix.tabulate_polynomials \
+( basix.PolynomialType.legendre
+, basix.CellType.triangle
+, 2
+, pts )
+
+wcoeffs = np.array \
+( [ ((f(pts[:, 0], pts[:, 1])*wts)[None, :]*poly).sum(axis=-1) for f in
+    [ lambda x, y: y*(1-y)
+    , lambda x, y: x*y
+    , lambda x, y: x*y
+    , lambda x, y: x*(1-x) ] ]
+, dtype=np.float64) \
+.reshape(2, -1)
+
+x = [[], [], [], []]
+for i, _ in itertools.product(range(2), range(3)):
+  x[i].append(np.zeros((0, 2)))
+x[2].append(np.array([[0.5, 0.0], [0.0, 0.5]]))
+
+M = [[], [], [], []]
+for i, _ in itertools.product(range(2), range(3)):
+  M[i].append(np.zeros((0, 2, 0, 1)))
+M[2].append \
+( np.array
+  ( [ [[[4.]], [[0.]]]
+    , [[[0.]], [[4.]]] ] ) )
+
+element = basix.create_custom_element \
+( basix.CellType.triangle
+, [2], wcoeffs, x, M, 0
+, basix.MapType.covariantPiola
+, basix.SobolevSpace.HCurl
+, True, 1, 2 )
