f9aa31a3fbef47ce94817286a5b8483e1e27b35e
Takuya Yamauchi <tyama@preferred.jp>
2023-08-13 17:52:25 +0900

    Plotted.

--- fenicsx/p07.py
+++ fenicsx/p07.py
@@ -3,6 +3,7 @@ import dolfinx
 import ufl
 import mpi4py
 import petsc4py
+import pyvista
 
 print(petsc4py.PETSc.ScalarType)
 
@@ -39,3 +40,23 @@ L = ufl.inner(0*n[1]-1*n[0], hc_ts[0]*n[1]-hc_ts[1]*n[0])*ds(0)
 problem = dolfinx.fem.petsc.LinearProblem(a, L, [dirichlet_bc])
 sol = problem.solve()
 print(sol.x.array)
+
+l2_2d = dolfinx.fem.VectorFunctionSpace(msh, ("DG", 2))
+l2_2d_f = dolfinx.fem.Function(l2_2d)
+l2_2d_f.interpolate(sol)
+
+cells, cell_types, x = dolfinx.plot.create_vtk_mesh(l2_2d)
+grid = pyvista.UnstructuredGrid(cells, cell_types, x)
+grid.point_data["l2_2d_f"] = np.hstack \
+( [ l2_2d_f.x.array.reshape \
+    ( x.shape[0], l2_2d.dofmap.index_map_bs )
+  , np.zeros((x.shape[0], 1)) ] )
+glyphs = grid.glyph(orient="l2_2d_f", factor=0.5)
+
+plotter = pyvista.Plotter()
+plotter.add_mesh \
+( pyvista.UnstructuredGrid(*dolfinx.plot.create_vtk_mesh(msh))
+, style="wireframe", line_width=2, color="black" )
+plotter.add_mesh(glyphs)
+plotter.view_xy()
+plotter.show()
