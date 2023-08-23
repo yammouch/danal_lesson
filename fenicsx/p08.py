8792086feafeeff22583ecb9d6cbc0630ed4df8f
Takuya Yamauchi <tyama@preferred.jp>
2023-08-23 17:51:00 +0900

    Prints mesh data.

--- fenicsx/p08.py
+++ fenicsx/p08.py
@@ -2,18 +2,27 @@ import gmsh
 
 gmsh.initialize()
 
-p = [gmsh.model.occ.addPoint(x, y, 0, 3) for x, y in [[0, -1], [0, 1], [2, 0]]]
+p = \
+[ gmsh.model.occ.addPoint(x, y, 0) for x, y in
+  [ [0, 0], [1, 0], [1, 1], [0, 1] ] ]
 l = \
 [ gmsh.model.occ.addLine(i, j) for i, j in
   [ [p[0], p[1]]
   , [p[1], p[2]]
-  , [p[2], p[0]] ] ]
-c = gmsh.model.occ.addCurveLoop([l[0], l[1], l[2]])
+  , [p[2], p[3]]
+  , [p[3], p[0]] ] ]
+c = gmsh.model.occ.addCurveLoop([l[0], l[1], l[2], l[3]])
 s = gmsh.model.occ.addPlaneSurface([c])
+p = gmsh.model.addPhysicalGroup(2, [s])
 
 gmsh.model.occ.synchronize()
 
+gmsh.option.setNumber("Mesh.Algorithm", 8)
+gmsh.model.mesh.setSize(gmsh.model.getEntities(0), 3)
 gmsh.model.mesh.generate(2)
+print(gmsh.model.mesh.getElementsByType(2))
+print(gmsh.model.mesh.getNodesByElementType(2))
+print(gmsh.model.mesh.getNodes())
 
 gmsh.write("p08.geo_unrolled")
 gmsh.write("p08.brep")
