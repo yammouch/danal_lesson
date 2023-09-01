67e3b19992fe8cd418b07ffd900325fd19e5c163
Takuya Yamauchi <tyama@preferred.jp>
2023-09-01 12:43:43 +0900

    First checkin.

---
+++ fenicsx/p11.py
@@ -0,0 +1,119 @@
+import numpy as np
+import scipy.constants
+import gmsh
+import dolfinx
+import ufl
+import mpi4py
+import basix
+import pyvista
+import petsc4py
+import p06
+import p10
+
+msh = None
+hc = None
+
+def main():
+    global msh
+    global hc
+    mshg = p10.Mesh()
+
+    print(petsc4py.PETSc.ScalarType)
+
+    msh = mshg.msh[0]
+
+    hc = dolfinx.fem.FunctionSpace(msh, ('N1curl', 1))
+
+    mt = mshg.msh[2]
+
+    dirichlet_f = dolfinx.fem.Function(hc)
+    dirichlet_f.x.array[:] = 0
+
+    dirichlet_bc = dolfinx.fem.dirichletbc \
+    ( dirichlet_f
+    , ( np.sort
+        ( np.hstack
+          ( [ dolfinx.fem.locate_dofs_topological(hc, 1, [i])
+              for i in mt.find(mshg.pec) ] ) ) ) )
+
+    ds = ufl.Measure('ds', msh, subdomain_data=mt)
+    hc_tr = ufl.TrialFunction(hc)
+    hc_ts = ufl.TestFunction(hc)
+    n = ufl.FacetNormal(msh)
+    w = 2*scipy.constants.pi*1
+    a = ufl.inner(ufl.curl(hc_tr), ufl.curl(hc_ts))/scipy.constants.mu_0*ufl.dx
+    a -= w**2*scipy.constants.epsilon_0*ufl.inner(hc_tr, hc_ts)*ufl.dx
+    L = ufl.inner(0*n[1]-1*n[0], hc_ts[0]*n[1]-hc_ts[1]*n[0])*-w*ds(mshg.isrc)
+    problem = dolfinx.fem.petsc.LinearProblem(a, L, [dirichlet_bc])
+    sol = problem.solve()
+    print(sol.x.array)
+
+    l2_2d = dolfinx.fem.VectorFunctionSpace(msh, ("DG", 2))
+    l2_2d_f = dolfinx.fem.Function(l2_2d)
+    l2_2d_f.interpolate(sol)
+
+    er = dolfinx.fem.FunctionSpace \
+    ( msh
+    , basix.ufl_wrapper.BasixElement(p06.element) )
+    er_tr = ufl.TrialFunction(er)
+    er_ts = ufl.TestFunction(er)
+
+    a_er = ufl.inner(ufl.curl(er_tr), ufl.curl(er_ts))/scipy.constants.mu_0*ufl.dx
+    a_er -= w**2*scipy.constants.epsilon_0*ufl.inner(er_tr, er_ts)*ufl.dx
+    L_er = -ufl.inner(ufl.curl(sol), ufl.curl(er_ts))/scipy.constants.mu_0*ufl.dx
+    L_er += w**2*scipy.constants.epsilon_0*ufl.inner(sol, er_ts)*ufl.dx
+    L_er -= ufl.inner(0*n[1]-1*n[0], er_ts[0]*n[1]-er_ts[1]*n[0])*-w*ds(mshg.isrc)
+    L_er -= ufl.inner( n[1]*ufl.curl(sol), er_ts[0])/scipy.constants.mu_0*ds(mshg.pec)
+    L_er -= ufl.inner(-n[0]*ufl.curl(sol), er_ts[1])/scipy.constants.mu_0*ds(mshg.pec)
+    L_er -= ufl.inner \
+    (   n("+")[1]
+      * 0.5
+      * (ufl.curl(sol)("+") + ufl.curl(sol)("-"))
+    , er_ts("+")[0] )/scipy.constants.mu_0*ufl.dS
+    L_er -= ufl.inner \
+    (   n("-")[1]
+      * 0.5
+      * (ufl.curl(sol)("+") + ufl.curl(sol)("-"))
+    , er_ts("-")[0] )/scipy.constants.mu_0*ufl.dS
+    problem_er = dolfinx.fem.petsc.LinearProblem(a_er, L_er, [])
+    sol_er = problem_er.solve()
+    l2_2d_er = dolfinx.fem.Function(l2_2d)
+    l2_2d_er.interpolate(sol_er)
+    print(sol_er.x.array)
+
+    cells, cell_types, x = dolfinx.plot.create_vtk_mesh(l2_2d)
+
+    plotter = pyvista.Plotter(shape=(1, 2))
+
+    grid = pyvista.UnstructuredGrid(cells, cell_types, x)
+    grid.point_data["l2_2d_f"] = np.hstack \
+    ( [ l2_2d_f.x.array.reshape \
+        ( x.shape[0], l2_2d.dofmap.index_map_bs )
+      , np.zeros((x.shape[0], 1)) ] )
+    glyphs = grid.glyph(orient="l2_2d_f", factor=1e5)
+
+    plotter.subplot(0, 0)
+    plotter.add_mesh \
+    ( pyvista.UnstructuredGrid(*dolfinx.plot.create_vtk_mesh(msh))
+    , style="wireframe", line_width=2, color="black" )
+    plotter.add_mesh(glyphs)
+    plotter.view_xy()
+
+    grid1 = pyvista.UnstructuredGrid(cells, cell_types, x)
+    grid1.point_data["l2_2d_er"] = np.hstack \
+    ( [ l2_2d_er.x.array.reshape \
+        ( x.shape[0], l2_2d.dofmap.index_map_bs )
+      , np.zeros((x.shape[0], 1)) ] )
+    glyphs1 = grid1.glyph(orient="l2_2d_er", factor=1e5)
+
+    plotter.subplot(0, 1)
+    plotter.add_mesh \
+    ( pyvista.UnstructuredGrid(*dolfinx.plot.create_vtk_mesh(msh))
+    , style="wireframe", line_width=2, color="black" )
+    plotter.add_mesh(glyphs1)
+    plotter.view_xy()
+
+    plotter.show()
+
+if __name__ == "__main__":
+    main()
