10daa93157fbbcb016057a2b896bf257061fda89
Takuya Yamauchi <tyama@preferred.jp>
2025-03-26 17:52:07 +0900

    updown2note_build() and note2onoff_build() applied.

--- reson/www/press2note.js
+++ reson/www/press2note.js
@@ -18,38 +18,6 @@ export function note2onoff_build(notekey) {
   });
 }
 
-class press2note {
-
-  constructor() {
-    this.pressed = {};
-    this.pressed[4] = {"KeyZ": false, "Digit2": false}
-    this.press2note = {};
-    this.press2note["KeyZ"] = "4";
-    this.press2note["Digit2"] = "4";
-  }
-
-  on(key) {
-    const note = this.press2note[key];
-    console.log(note);
-    if (!note) { return; }
-    console.log(this.pressed[note]);
-    if (this.pressed[note][key]) { return; }
-    this.pressed[note][key] = true;
-    return { cmd: "on", note: note };
-  }
-
-  off(key) {
-    const note = this.press2note[key];
-    if (!note) { return; }
-    this.pressed[note][key] = false;
-    if (Object.entries(this.pressed[note]).some((k) => k[1])) { return; }
-    return { cmd: "off", note: note };
-  }
-
-}
-
-export const pressed = new press2note();
-
 let notekey = [
   ["KeyQ"],
   ["ShiftLeft", "Digit2"],
@@ -90,3 +58,32 @@ let notekey = [
   ["Enter"],
   ["ShiftRight"]
 ];
+
+class press2note {
+
+  constructor() {
+    this.pressed = note2onoff_build(notekey);
+    this.press2note = updown2note_build(notekey);
+  }
+
+  on(key) {
+    const note = this.press2note[key];
+    console.log(note);
+    if (!note) { return; }
+    console.log(this.pressed[note]);
+    if (this.pressed[note][key]) { return; }
+    this.pressed[note][key] = true;
+    return { cmd: "on", note: note };
+  }
+
+  off(key) {
+    const note = this.press2note[key];
+    if (!note) { return; }
+    this.pressed[note][key] = false;
+    if (Object.entries(this.pressed[note]).some((k) => k[1])) { return; }
+    return { cmd: "off", note: note };
+  }
+
+}
+
+export const pressed = new press2note();
